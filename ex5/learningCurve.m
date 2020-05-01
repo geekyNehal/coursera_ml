c26d0acc78259c865215219f0d7b936bd763c2de
Nehal Kumar Singh <nehaljohn91@gmail.com>
2020-05-02 03:11:05 +0530

    week 5 task two completed

--- ex5/learningCurve.m
+++ ex5/learningCurve.m
@@ -53,13 +53,23 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+for i = 1:m
+  Xtrain = X(1:i,:);
+	ytrain = y(1:i);
+  
+  theta = trainLinearReg(Xtrain, ytrain, lambda);	
+  
+  [Jtrain,grad_train]=linearRegCostFunction(Xtrain,ytrain,theta,0); 
+	[Jval,grad_val]=linearRegCostFunction(Xval,yval,theta,0);
+  
+  error_train(i) = Jtrain;  
+  error_val(i) = Jval;
+   
 
-
-
-
-
+end
 
 % -------------------------------------------------------------
+
 
 % =========================================================================
 
